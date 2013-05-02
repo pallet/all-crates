f5e004fa7e3fa98927b9e99fd02191811fa76f29
Hugo Duncan <hugo@hugoduncan.org>
2013-05-02 13:27:05 -0400

    Move snapshots profile to profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,1 +1,11 @@
-{:dev {:plugins [[com.palletops/lein-pallet-crate "0.1.1"]]}}
+{:dev {:plugins [[com.palletops/lein-pallet-crate "0.1.1"]]}
+ :snapshots
+ {:dependencies [[com.palletops/app-deploy-crate "0.8.0-SNAPSHOT"]
+                 [com.palletops/java-crate "0.8.0-SNAPSHOT"]
+                 [com.palletops/lein-crate "0.8.0-SNAPSHOT"]
+                 [com.palletops/git-crate "0.8.0-SNAPSHOT"]
+                 [com.palletops/riemann-crate "0.8.0-SNAPSHOT"]
+                 [com.palletops/upstart-crate "0.8.0-SNAPSHOT"]
+                 [com.palletops/runit-crate "0.8.0-SNAPSHOT"]
+                 [com.palletops/ami-crate "0.8.0-SNAPSHOT"]
+                 [com.palletops/rbenv-crate "0.8.0-SNAPSHOT"]]}}
--- project.clj
+++ project.clj
@@ -12,17 +12,6 @@
                  [com.palletops/runit-crate "0.8.0-alpha.1"]
                  [com.palletops/ami-crate "0.8.0-alpha.1"]
                  [com.palletops/rbenv-crate "0.8.0-alpha.1"]]
-  :profiles
-  {:snapshots
-   {:dependencies [[com.palletops/app-deploy-crate "0.8.0-SNAPSHOT"]
-                   [com.palletops/java-crate "0.8.0-SNAPSHOT"]
-                   [com.palletops/lein-crate "0.8.0-SNAPSHOT"]
-                   [com.palletops/git-crate "0.8.0-SNAPSHOT"]
-                   [com.palletops/riemann-crate "0.8.0-SNAPSHOT"]
-                   [com.palletops/upstart-crate "0.8.0-SNAPSHOT"]
-                   [com.palletops/runit-crate "0.8.0-SNAPSHOT"]
-                   [com.palletops/ami-crate "0.8.0-SNAPSHOT"]
-                   [com.palletops/rbenv-crate "0.8.0-SNAPSHOT"]]}}
   :prep-tasks ["crates-pages"]
   :classifiers {:meta {:dependencies ^:replace []
                        :source-paths ^:replace []
